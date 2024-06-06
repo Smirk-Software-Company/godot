9baded89c41bdafcbd0af4850eac36527ea9707a
jbromberg <58891459+jbromberg@users.noreply.github.com>
2024-06-05 20:39:23 -0700

    update kinematic body

--- scene/3d/physics/kinematic_body_3d.cpp
+++ scene/3d/physics/kinematic_body_3d.cpp
@@ -8,38 +8,23 @@ Vector3 KinematicBody3D::get_linear_velocity() const {
 	return linear_velocity;
 }
 
-void KinematicBody3D::set_surface_linear_velocity(const Vector3 &p_velocity) {
-	surface_linear_velocity = p_velocity;
-}
-
-Vector3 KinematicBody3D::get_surface_linear_velocity() const {
-	return surface_linear_velocity;
-}
-
-void KinematicBody3D::set_surface_angular_velocity(const Vector3 &p_velocity) {
-	surface_angular_velocity = p_velocity;
-}
-
-Vector3 KinematicBody3D::get_surface_angular_velocity() const {
-	return surface_angular_velocity;
-}
-
 void KinematicBody3D::_notification(int p_what) {
 	switch (p_what) {
-		case NOTIFICATION_ENTER_TREE: {
-			last_transform = get_global_transform();
-		} break;
-
 		case NOTIFICATION_PHYSICS_PROCESS: {
 			Transform3D transform = get_global_transform();
-
-			PhysicsServer3D::get_singleton()->body_set_state(get_rid(), PhysicsServer3D::BODY_STATE_LINEAR_VELOCITY, ((transform.origin - last_transform.origin) / get_physics_process_delta_time()) + surface_linear_velocity);
-			PhysicsServer3D::get_singleton()->body_set_state(get_rid(), PhysicsServer3D::BODY_STATE_ANGULAR_VELOCITY, ((transform.basis * last_transform.basis.inverse()).get_euler() / get_physics_process_delta_time()) + surface_angular_velocity);
-
-			last_transform = transform;
-
 			set_global_transform(Transform3D(transform.basis, transform.origin + linear_velocity * get_physics_process_delta_time()));
 			_on_transform_changed();
+		} break;
+
+		case NOTIFICATION_CHILD_ORDER_CHANGED: {
+			for (int i = 0; i < get_child_count(false); i++) {
+				Variant custom_class_name = get_child(i, false)->get("custom_class_name");
+
+				if (custom_class_name && custom_class_name.get_type() == Variant::STRING && custom_class_name == "LuaScript") {
+					PhysicsServer3D::get_singleton()->body_set_mode(get_rid(), PhysicsServer3D::BODY_MODE_KINEMATIC);
+					break;
+				}
+			}
 		} break;
 	}
 }
@@ -47,14 +32,8 @@ void KinematicBody3D::_notification(int p_what) {
 void KinematicBody3D::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_linear_velocity", "linear_velocity"), &KinematicBody3D::set_linear_velocity);
 	ClassDB::bind_method(D_METHOD("get_linear_velocity"), &KinematicBody3D::get_linear_velocity);
-	ClassDB::bind_method(D_METHOD("set_surface_linear_velocity", "linear_velocity"), &KinematicBody3D::set_surface_linear_velocity);
-	ClassDB::bind_method(D_METHOD("get_surface_linear_velocity"), &KinematicBody3D::get_surface_linear_velocity);
-	ClassDB::bind_method(D_METHOD("set_surface_angular_velocity", "linear_velocity"), &KinematicBody3D::set_surface_angular_velocity);
-	ClassDB::bind_method(D_METHOD("get_surface_angular_velocity"), &KinematicBody3D::get_surface_angular_velocity);
 
 	ADD_PROPERTY(PropertyInfo(Variant::VECTOR3, "linear_velocity", PROPERTY_HINT_NONE, "suffix:m/s"), "set_linear_velocity", "get_linear_velocity");
-	ADD_PROPERTY(PropertyInfo(Variant::VECTOR3, "surface_linear_velocity", PROPERTY_HINT_NONE, "suffix:m/s"), "set_surface_linear_velocity", "get_surface_linear_velocity");
-	ADD_PROPERTY(PropertyInfo(Variant::VECTOR3, "surface_angular_velocity", PROPERTY_HINT_NONE, "suffix:m/s"), "set_surface_angular_velocity", "get_surface_angular_velocity");
 }
 
 KinematicBody3D::KinematicBody3D() :
--- scene/3d/physics/kinematic_body_3d.h
+++ scene/3d/physics/kinematic_body_3d.h
@@ -5,10 +5,6 @@ class KinematicBody3D : public StaticBody3D {
 
 private:
 	Vector3 linear_velocity;
-	Vector3 surface_linear_velocity;
-	Vector3 surface_angular_velocity;
-
-	Transform3D last_transform;
 
 protected:
 	void _notification(int p_what);
@@ -17,10 +13,6 @@ protected:
 public:
 	void set_linear_velocity(const Vector3 &p_velocity);
 	Vector3 get_linear_velocity() const override;
-	void set_surface_linear_velocity(const Vector3 &p_velocity);
-	Vector3 get_surface_linear_velocity() const;
-	void set_surface_angular_velocity(const Vector3 &p_velocity);
-	Vector3 get_surface_angular_velocity() const;
 
 	KinematicBody3D();
 
